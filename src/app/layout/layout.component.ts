c2a72aead0c3cafadc60d4652e2b8ad5756d92ff
antmusumba <musumbaantony3@gmail.com>
2025-01-07 09:51:45 +0300

    feat: add sidebar component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent,SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
